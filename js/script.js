bbc42aa24a6c038a076333106b038ee147394168
martemo <marte.moslet@hotmail.com>
2022-03-31 21:43:48 +0200

    Update script

---
+++ js/script.js
@@ -0,0 +1,48 @@
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+// question 1
+
+const cat = {
+    complain: "Meow!"
+};
+
+console.log(cat)
+
+// question 2
+
+const heading = document.querySelector("h3");
+heading.innerHTML = "Updated heading";
+
+// question 3
+
+
+
+// question 3
+
+
+
+// question 5
+
+
+
+// question 6
+
+
+
+// question 7
+
+
+
+// question 8
