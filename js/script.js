1633b0480f91fd34e1f51dedc0e58666fd9ca4fb
martemo <marte.moslet@hotmail.com>
2022-04-01 17:58:02 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -27,22 +27,37 @@ heading.innerHTML = "Updated heading";
 
 // question 3
 
+heading.style.fontSize = "2em";
 
+// question 4
 
-// question 3
-
-
+heading.classList.add("subheading");
 
 // question 5
 
+document.querySelectorAll("p");
 
+const paragraphs = document.querySelectorAll("p");
+
+for(let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = "red";
+}
 
 // question 6
 
-
+const resultsContainer = document.querySelector(".results")
+   
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+resultsContainer.style.backgroundColor = "yellow";
 
 // question 7
 
+function createList(list) {
+    for(let i = 0; i < list.length; i++) {
+        console.log(list[i]);
+    }
+}
 
+createList(cats);
 
 // question 8
